0c59501255fbd3452e17152c197beb08b7236bf4
Vlad <vladggordiychuk>
2019-06-12 10:50:39 +0300

    Initial Commit

---
+++ lab5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab5
+//
+//  Created by Vlad Gordiichuk on 6/12/19.
+//  Copyright © 2019 Vlad Gordiichuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
